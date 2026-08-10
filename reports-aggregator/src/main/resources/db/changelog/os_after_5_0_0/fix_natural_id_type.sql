614c9a40393bf93ed53b5dea7e03dedf0cbb0efd
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2026-08-10 18:02:44 +0400

    fix: Fix migration script for natural_id column.

--- reports-aggregator/src/main/resources/db/changelog/os_after_5_0_0/fix_natural_id_type.sql
+++ reports-aggregator/src/main/resources/db/changelog/os_after_5_0_0/fix_natural_id_type.sql
@@ -1,18 +1,24 @@
 
-ALTER TABLE mb_tccontext ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+-- ALTER TABLE mb_tccontext ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+ALTER TABLE mb_tccontext ALTER COLUMN natural_id TYPE character varying(255) USING natural_id::varchar(255);
 commit;
 
-ALTER TABLE mb_context ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+-- ALTER TABLE mb_context ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+ALTER TABLE mb_context ALTER COLUMN natural_id TYPE character varying(255) USING natural_id::varchar(255);
 commit;
 
-ALTER TABLE mb_upgrade_history ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+-- ALTER TABLE mb_upgrade_history ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+ALTER TABLE mb_upgrade_history ALTER COLUMN natural_id TYPE character varying(255) USING natural_id::varchar(255);
 commit;
 
-ALTER TABLE mb_instance ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+-- ALTER TABLE mb_instance ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+ALTER TABLE mb_instance ALTER COLUMN natural_id TYPE character varying(255) USING natural_id::varchar(255);
 commit;
 
-ALTER TABLE mb_message ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+-- ALTER TABLE mb_message ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+ALTER TABLE mb_message ALTER COLUMN natural_id TYPE character varying(255) USING natural_id::varchar(255);
 commit;
 
-ALTER TABLE mb_message_param ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+-- ALTER TABLE mb_message_param ALTER COLUMN natural_id TYPE bigint USING natural_id::bigint;
+ALTER TABLE mb_message_param ALTER COLUMN natural_id TYPE character varying(255) USING natural_id::varchar(255);
 commit;
